60d75ceb2d3ac3bb7df2ec9ee377688f3b81acdd
Philipp Kessling <p.kessling@leibniz-hbi.de>
2024-11-26 12:46:56 +0100

    Adjust whitespace to our liking.

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,4 +1,8 @@
+"""Test suite for the package-level items."""
+
 from dabapush_gbq import __version__
 
+
 def test_version():
+    """Should be the correct version of the package."""
     assert __version__ == "0.1.0"
